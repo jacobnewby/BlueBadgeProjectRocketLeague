8588c7f8dfb1169e6f6094548b290564cc33aca4
jacobnewby <jacobnewby01@gmail.com>
2019-10-28 11:01:57 -0400

    update

--- RocketLeague.Models/BuildEdit.cs
+++ RocketLeague.Models/BuildEdit.cs
@@ -4,12 +4,14 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace RocketLeague.Models
 {
     public class BuildEdit
     {
         public int BuildID { get; set; }
+        public HttpPostedFileBase File { get; set; }
         [Display(Name = "Name")]
         public string BuildName { get; set; }
         [Display(Name = "Car")]
